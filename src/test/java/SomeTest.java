556f216cff2e638bec25c0b45e7550ff48045cb8
maxime.dezette <m.dezette@gmail.com>
2021-10-10 10:08:41 +0200

    Initial commit :tada:

---
+++ src/test/java/SomeTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class SomeTest {
+
+    @Test
+    void shouldBeTrue(){
+        assertThat(true).isTrue();
+    }
+}
